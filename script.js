0a61207202780027d5201ef25dcb7551b63a9e0e
faaln <34640250+faaln@users.noreply.github.com>
2022-07-17 12:39:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for (let count = 1; count <= 10; count += 1) {
+    console.log(count);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+count = 10;
+
+while (count >= 1) {
+    console.log(count);
+    count -= 1;
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for (count = 1; count <= 100; count += 2) {
+    console.log(count);
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+count = 0
+
+while(count<=100){
+    console.log(count)
+    count+=2
+}
